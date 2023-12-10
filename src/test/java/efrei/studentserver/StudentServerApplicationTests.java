40a213bd20aebed8c89ad5712d6ba98b1e7ccb85
Yannis94-efrei <yannis.mouisset-ferrara@efrei.net>
2023-12-10 22:04:54 +0100

    lancement projet par défaut

---
+++ src/test/java/efrei/studentserver/StudentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package efrei.studentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
